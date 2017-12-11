ba9ef7f1c4a31e8a5dc8ae35170c3e28aac4bedf
Joe Edwards <maddo4.dev@gmail.com>
2017-12-11 00:40:09 +0000

    Added extra field to migration for users
    - Added the year_group and type to the initial users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('type');
-            
+            $table->integer('year_group');
             $table->timestamps();
         });
     }
