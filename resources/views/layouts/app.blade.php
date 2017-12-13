c8af47abbd0e5a87352cc956fd6f364b9dbf4659
Joe Edwards <maddo4.dev@gmail.com>
2017-12-13 09:56:16 +0000

    Added style to the app
    - Added all required fonts from bootstrap 3
    - Added DHSB Aleo font
    - Added a new layout for just blank pages
    - Modified app.blade to include the new css files
    - Trigured laravel mix
    - Added some styles to app.scss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,32 @@
 	<title>Refuel</title>
 	@yield('header')
 
+	<link rel="stylesheet" type="text/css" href="css/app.css">
+
 </head>
 <body>
+
+	<div class="navbar">
+		
+		<div class="navbar-brand container-fluid">
+			
+			<h3 class="center" id="logo">Refuel</h3>
+			<p class="center" id="logo-sub">Ordering Sytem</p>
+
+		</div>
+		<ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+
+	</div>
 
 	@yield('content')
 
---
+++ resources/views/layouts/blank.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Error</title>
+
+	<link rel="stylesheet" type="text/css" href="/css/app.css">
+</head>
+<body>
+	@yield('content')
+</body>
+</html>
