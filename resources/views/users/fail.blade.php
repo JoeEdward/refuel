dffd2b53a1243a06bc740180907d34359fffd3a4
Joe Edwards <maddo4.dev@gmail.com>
2017-12-13 09:44:44 +0000

    Changed Error Pages
    -Added the 403 error page and changed the 404 page

---
+++ resources/views/users/fail.blade.php
@@ -0,0 +1,16 @@
+
+@extends('layouts.blank')
+	
+@section('content')
+	<h1 class="col-md-12 title" style="text-align: center;">HTTP ERROR 403: Forbidden</h1>
+
+	<h1 class="col-md-12 title" style="text-align: center;">You don't have access to this page</h1>
+
+	<p class="col-md-12 subtitle" style="text-align: center;">Please log in below</p>
+
+	<div class="col-md-12">
+	<a class="col-md-12 center" href="/login"><img class="google-button" src="/img/googlebutton.png" style="text-align: center;"></a>
+</div>
+
+@endsection
+
