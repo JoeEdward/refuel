aa125639b5b6e8df64c957d9e7c181074e9218f2
Joe Edwards <maddo4.dev@gmail.com>
2018-01-09 09:25:58 +0000

    Updated user model with new fields

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','type','year_group',
+        'name', 'email', 'password','type','year_group', 'allow',
     ];
 
     /**
