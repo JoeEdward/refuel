4962d61062833a058b509892db4fd0ee5db8c72b
Joe Edwards <maddo4.dev@gmail.com>
2018-01-09 09:26:31 +0000

    Added custom Middleware
    Added a peice of middleware that check if the current user is staff or not

--- resources/views/users/fail.blade.php
+++ resources/views/users/fail.blade.php
@@ -6,7 +6,7 @@
 
 	<h1 class="col-md-12 title" style="text-align: center;">You don't have access to this page</h1>
 
-	<p class="col-md-12 subtitle" style="text-align: center;">Please log in below</p>
+	<p class="col-md-12 subtitle" style="text-align: center;">Please log in with an account with higher access rights below</p>
 
 	<div class="col-md-12">
 	<a class="col-md-12 center" href="/login"><img class="google-button" src="/img/googlebutton.png" style="text-align: center;"></a>
